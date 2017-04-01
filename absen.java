d6981fba5da5d8443a4d14e776c60567071704a1
soviandony98 <soviandony98@gmail.com>
2017-04-01 22:52:21 +0700

    mengedit file absen

--- absen.java
+++ absen.java
@@ -12,33 +12,22 @@ public class absen {
         
         users[0] = "syaifudin";
 	passwords[0] = "syaifudin";
-	users[1] = "anung";
-	passwords [1] = "anung";
        
         Scanner akun = new Scanner(System. in );
-	Scanner akun1 = new Scanner(System. in );
-
-
        
         System.out.println("masukkan Username: ");
         user = akun.nextLine();
         System.out.println("masukkan Password: ");
         password = akun.nextLine();
 
-	System.out.println("masukkan Username: ");
-        user = akun1.nextLine();
-        System.out.println("masukkan Password: ");
-        password = akun1.nextLine();
 
-				
-		if (i=0; i<=3; i++)
-					{
+					
         if (users[i].equals(user) && passwords[i].equals(password)) {
             System.out.println("Login Sukses");
         } else {
             System.out.println("Login Gagal");
         }
-					}
+					
 				
     }
 }
